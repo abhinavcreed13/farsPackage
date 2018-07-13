9aa58ac6833e35244f734669442345b5dd27b911
Abhinav Sharma <abhinavcs13@gmail.com>
2018-07-13 12:18:45 +0530

    Fixing test case

--- tests/testthat/test-make_filename.R
+++ tests/testthat/test-make_filename.R
@@ -2,12 +2,12 @@ context("make_filename")
 
 test_that("make_filename works", {
   input <- "2013"
-  output <- "data/accident_2013.csv.bz2"
+  output <- "accident_2013.csv.bz2"
   expect_equal(make_filename(input),output)
   input <- "2014"
-  output <- "data/accident_2014.csv.bz2"
+  output <- "accident_2014.csv.bz2"
   expect_equal(make_filename(input),output)
   input <- 2015
-  output <- "data/accident_2015.csv.bz2"
+  output <- "accident_2015.csv.bz2"
   expect_equal(make_filename(input),output)
 })
