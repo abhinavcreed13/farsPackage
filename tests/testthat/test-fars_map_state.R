cff8c64829b36b5e008e0c78e2e0a378fc0eefa9
Abhinav Sharma <abhinavcs13@gmail.com>
2018-07-13 13:29:47 +0530

    Removing warnings

--- tests/testthat/test-fars_map_state.R
+++ tests/testthat/test-fars_map_state.R
@@ -1,7 +1,7 @@
 context("fars_map_state")
 
 test_that("fars_map_state works", {
-  fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "FARSread")
+  fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "farsPackage")
   file.copy(from = fars_2013, to = getwd())
   Texas <- fars_map_state(48, 2013)
   expect_null(Texas, "null")
--- tests/testthat/test-fars_summarize_years.R
+++ tests/testthat/test-fars_summarize_years.R
@@ -1,7 +1,7 @@
 context("fars_summarize_years")
 
 test_that("fars_summarize_years generates warning", {
-  fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "FARSread")
+  fars_2013 <- system.file("extdata", "accident_2013.csv.bz2", package = "farsPackage")
   file.copy(from = fars_2013, to = getwd())
   expect_that(fars_summarize_years(c(2013, "ABCD")), gives_warning())
   file.remove("./accident_2013.csv.bz2")
